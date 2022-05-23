6c9c899faea2fea9fc9e9173aa391b4ae31762ab
nicopardom <npardom@unal.edu.co>
2022-05-22 20:36:32 -0500

    full GUI's

--- chazam/apps/base/views.py
+++ chazam/apps/base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'goToLogin.html')
 
 @login_required
 def mainPage(request):
