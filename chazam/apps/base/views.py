fda27ed7a4183144de8394080f297e042b8ed8bd
nicopardom <npardom@unal.edu.co>
2022-05-23 00:16:56 -0500

    hello

--- chazam/apps/base/views.py
+++ chazam/apps/base/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import comensales
+
 # Create your views here.
 def login(request):
     return render(request, 'goToLogin.html')
+
+def finalSignup(request):
+    idActual = str(request.user.id)
+    o = comensales.objects.raw("SELECT * from base_comensales where IdTipoUsuario_id = "+idActual)
+    count = 0
+    for obj in o:
+        count += 1
+    if count != 0:
+        return render(request, 'mainPage.html')
+    else:
+        return render(request, 'finalSignup.html')
 
 @login_required
 def mainPage(request):
