b452c2f3b7a38c69318fb0a22643395a490fa81d
Daniela Tocua <dtocua@unal.edu.co>
2022-06-27 23:14:19 -0500

    a

--- apps/base/views.py
+++ apps/base/views.py
@@ -26,7 +26,7 @@ def login(request):
     
 def finalSignup(request):
     idActual = str(request.user.id)
-    o = comensales.objects.raw("SELECT * from base_comensales.RegistroFinal where RegistroFinal = "+idActual)
+    o = comensales.objects.raw("SELECT * from base_comensales where RegistroFinal = "+idActual)
     count = 0
     for obj in o:
         count += 1
